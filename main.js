30d1acea76e6530f32b6fbf55a75f290e2f1847c
Taylor Lang <lang.taylor.j@gmail.com>
2017-02-19 18:33:39 -0600

    first attempt

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
 // GO!
+var studentButton = document.querySelector('#studentButton')
+var	navMenu = document.querySelector('.nav-menu,a')
+var navState = {
+	showing: true,
+}
 
+studentButton.addEventListener('click', function() {
+	if (navState.showing) {
+		navMenu.style.opacity = '0'
+		navState.showing = false
+	}
+	else {
+		navMenu.style.opacity = '1'
+		navState.showing = true
+	}
+})
+
+var inputNode = document.querySelector('input[type="text"]')
+var guestNode = document.querySelector('.guest-list')
+
+inputNode.addEventListener('keydown', function(guest){
+  if (guest.keyCode === KEYCODES.enter) {
+  	guestNode.appendChild(guest.target.value)
+  }
+})	
